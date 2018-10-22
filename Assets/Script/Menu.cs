5f9839c966034d1ffa07dcf6d17f5dab0c74bda3
Yann <26348667+Mrplobi@users.noreply.github.com>
2018-10-22 19:56:08 +0200

    main menu et interactions

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour{
+    
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene("InstructionScene", LoadSceneMode.Single);
+    }
+
+    public void LoadFreeDetection()
+    {
+        SceneManager.LoadScene("DanceWaterDance", LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("quitting");
+    }
+}
